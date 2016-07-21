829abce6e30ca0c00c2976b90a76cbae54214184
Mosquit <ivos.k@atlas.cz>
2016-07-21 08:52:48 +0200

    forward/backward by keyboard works, no handling of end stations

--- Assets/Scripts/InitialCreations.cs
+++ Assets/Scripts/InitialCreations.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 using System.Collections.Generic;
 
 public class InitialCreations : MonoBehaviour {
@@ -10,12 +11,50 @@ public class InitialCreations : MonoBehaviour {
   int nextTrackPosition;
   bool at_destination = false;
 
+  float speed = 0;
+  float absSpeed = 0;
+  const float speedInc = 0.005f;
+  int direction;
+
+  void determineDirection() {
+    if (speed < 0) {
+      direction = -1;
+    } else  {
+      direction = 1;
+    }
+    absSpeed = Math.Abs(speed);
+
+  }
+
+  void determineNextTrackPosition() {
+    int expectedNext;
+    if (direction == 1) {
+      expectedNext = currentTrackPosition + direction;
+    } else {
+      expectedNext = currentTrackPosition;
+      // currentTrackPosition = currentTrackPosition + 1;
+    }
+
+    if (expectedNext < 0) expectedNext = 0;
+    if (expectedNext > track.Count - 1) expectedNext = track.Count -1;
+
+    nextTrackPosition = expectedNext;
+    if (currentTrackPosition == nextTrackPosition) {
+      at_destination = true;
+      Debug.Log("In target");
+    } else {
+      at_destination = false;
+    }
+  }
+
 	// Use this for initialization
 	void Start () {
 
        track = new List<Vector3>();
        currentTrackPosition = 0;
        nextTrackPosition = 0;
+       speed = 0f;
+       direction = 1;
         /*
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         cube.transform.position = new Vector3(0, 0.05F, 0);
@@ -49,7 +88,7 @@ public class InitialCreations : MonoBehaviour {
 
         // find nearest track point
         float minDist = float.MaxValue;
-        Vector3 cloasest = new Vector3(0,0,0);
+        // Vector3 cloasest = new Vector3(0,0,0);
         for (int i = 0; i < track.Count; i++) {
 
           float dist = Vector3.Distance(track[i], napr.transform.position);
@@ -57,13 +96,6 @@ public class InitialCreations : MonoBehaviour {
             minDist = dist;
             // cloasest = track[i];
             currentTrackPosition = i;
-
-
-            if(i == track.Count - 1) {
-              nextTrackPosition = currentTrackPosition;
-            } else {
-              nextTrackPosition = currentTrackPosition + 1;
-            }
           }
         }
 
@@ -77,37 +109,43 @@ public class InitialCreations : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        //Time.deltaTime
-        float step = 0.03f;
-
-        if(at_destination) return;
+        if (Input.GetKeyDown("up"))
+            speed += speedInc;
+        if (Input.GetKeyDown("down")) {
+            speed -= speedInc;
+        }
+        determineDirection();
+        determineNextTrackPosition();
 
         Vector3 oldPos = napr.transform.position;
-        Debug.Log("Heading towards index " + nextTrackPosition);
+        Debug.Log("Current = " +currentTrackPosition+  ", Heading towards index " + nextTrackPosition);
         Vector3 headingTowards = track[nextTrackPosition];
-        Vector3 newPos = Vector3.MoveTowards(oldPos, headingTowards, step);
+        Vector3 newPos = Vector3.MoveTowards(oldPos, headingTowards, absSpeed);
 
         float movedDistance = Vector3.Distance(oldPos, newPos);
         Debug.Log("Moved distance = " +  movedDistance);
 
-        float fullfill = calculateFullfillment(step, movedDistance);
-        if(fullfill < 0.95f) {
-          // we did not reached full step yet
-          currentTrackPosition++;
-          if(nextTrackPosition < track.Count - 1) {
-            nextTrackPosition++;
-          } else {
-            // already in last segment
-            at_destination = true;
-            Debug.Log("At destination...");
-          }
+        float fullfill = calculateFullfillment(absSpeed, movedDistance);
+        if(fullfill < 0.98f && fullfill > 0) {
+          // we did not reached full step, close to next track point. Have to move more...
+          currentTrackPosition += direction;
+          determineNextTrackPosition();
+          newPos = Vector3.MoveTowards(newPos, headingTowards, absSpeed - movedDistance);
+          //
+          // if(nextTrackPosition < track.Count - 1) {
+          //   nextTrackPosition++;
+          //   //TODO: this shall be in while cycle actually
+          //    headingTowards = track[nextTrackPosition];
+          //
+          //
+          // } else {
+          //   // already in last segment
+          //   at_destination = true;
+          //   Debug.Log("At destination...");
+          // }
 
         }
 
-        //float movedDistance = Vector3.Distance(napr.transform.position, headingTowards);
-        if(newPos == headingTowards) {
-          Debug.Log("In target");
-        }
         napr.transform.position = newPos;
         //napr.transform.position = newPos;
 	}
